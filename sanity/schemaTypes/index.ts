1bac85548ae3b939aa159047a0958735f5d1c535
ton1czech <tonyasek007@gmail.com>
2026-03-24 09:22:29 +0100

    🗑️ remove: old sanity schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,9 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
 import {eventType} from './eventType'
 import {localizedStringType} from './localizedStringType'
 import {localizedTextType} from './localizedTextType'
@@ -11,9 +8,6 @@ import {localizedTextType} from './localizedTextType'
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     blockContentType,
-    categoryType,
-    postType,
-    authorType,
     localizedStringType,
     localizedTextType,
     eventType,
