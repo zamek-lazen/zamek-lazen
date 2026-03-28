96d440f8e04ef1e59d6c448566976a934724a877
ton1czech <tonyasek007@gmail.com>
2026-03-28 11:39:38 +0100

    ✨ feat: sanity dashboard CZ

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
+import { csCZLocale } from '@sanity/locale-cs-cz'
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
@@ -20,6 +21,7 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
+    csCZLocale(),
     structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
